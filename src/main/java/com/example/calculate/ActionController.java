0682dd9032f8a728624ff30770f74fd8f96e10c5
FeralHorse <pusiket90@yandex.ru>
2023-11-07 19:33:09 +0300

    с проверкой на 0, и с проверкой на 2 знака

--- src/main/java/com/example/calculate/ActionController.java
+++ src/main/java/com/example/calculate/ActionController.java
@@ -7,26 +7,45 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ActionController {
     @GetMapping("/calculator/plus")
-    public String plus(@RequestParam("num1") int num1, @RequestParam("num2") int num2){
-        int res = num1 + num2;
-        return "Сумма равна: " + res;
+    public String plus(@RequestParam("num1") String num1, @RequestParam("num2") String num2){
+        if(checkNull(num1) && checkNull(num2)){
+            double res = Double.parseDouble(num1) + Double.parseDouble(num2);
+            return String.valueOf(res);
+        }
+        return "1 из параметров не введен";
+
     }
     @GetMapping("/calculator/minus")
-    public String minus(@RequestParam("num1") int num1, @RequestParam("num2") int num2){
-        int res = num1 - num2;
-        return "Разница равна: " + res;
+    public String minus(@RequestParam("num1") String num1, @RequestParam("num2") String num2){
+        if(checkNull(num1) && checkNull(num2)) {
+            double res = Double.parseDouble(num1) - Double.parseDouble(num2);
+            return String.valueOf(res);
+        }
+        return "1 из параметров не введен";
     }
     @GetMapping("/calculator/multiply")
-    public String multiply(@RequestParam("num1") int num1, @RequestParam("num2") int num2){
-        int res = num1 * num2;
-        return "Разница равна: " + res;
+    public String multiply(@RequestParam("num1") String num1, @RequestParam("num2") String num2){
+        if(checkNull(num1) && checkNull(num2)) {
+            double res = Double.parseDouble(num1) * Double.parseDouble(num2);
+            return String.valueOf(res);
+        }
+        return "1 из параметров не введен";
     }
     @GetMapping("/calculator/divide")
-    public String divide(@RequestParam("num1") int num1, @RequestParam("num2") int num2){
-        if(num2 != 0){
-            int res = num1 / num2;
-            return "Разница равна: " + res;
+    public String divide(@RequestParam("num1") String num1, @RequestParam("num2") String num2){
+        if(checkNull(num1) && checkNull(num2)) {
+            if(Integer.parseInt(num2) != 0){
+                double res = Double.parseDouble(num1) / Double.parseDouble(num2);
+                return String.valueOf(res);
+            }
+            return "Делить на 0 нельзя";
         }
-        return "Делить на 0 нельзя";
+        return "1 из параметров не введен";
+    }
+    private Boolean checkNull(String str){
+        if (str == null){
+            return false;
+        }
+        return true;
     }
 }
