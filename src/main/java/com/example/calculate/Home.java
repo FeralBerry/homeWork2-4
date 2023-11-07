64b5432a510de7312b7f66f94ffa7af081c99df2
FeralHorse <pusiket90@yandex.ru>
2023-11-07 12:32:14 +0300

    Initial commit

---
+++ src/main/java/com/example/calculate/Home.java
@@ -0,0 +1,10 @@
+package com.example.calculate;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class Home {
+    @GetMapping("/")
+    public void home(){
+
+    }
+}
---
+++ src/test/java/com/example/calculate/CalculateApplicationTests.java
@@ -0,0 +1,13 @@
+package test.java.com.example.calculate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
